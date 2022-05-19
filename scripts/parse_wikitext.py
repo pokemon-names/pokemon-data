e76d68299bbc744360d37a9c894b97a665d15b4b
Will Sheppard <nobody@example.com>
2022-05-19 19:05:10 +0000

    Remove example data

--- scripts/parse_wikitext.py
+++ scripts/parse_wikitext.py
@@ -45,18 +45,3 @@ for section in wikicode.get_sections(include_lead=False,flat=True):
 if debug: print("INTERESTING = "); pp.pp(interesting)
 
 exit(0)
-
-# __DATA__
-
-    # ====Origin===
-    # Pikachu is based on a {{wp|mouse}} after its name. Its cheek pouches were also inspired by {{wp|squirrel}}s, which store food in their cheeks. The stripes on its back and its lightning bolt-shaped tail were given for aesthetic reasons.<ref name=int/>
-    # 
-    # Pikachu's designer, [[Atsuko Nishida]], revealed in an interview that it was originally a {{wp|daifuku}}-like creature with ears. Its black ear tips are remnants from this original concept.<ref name=int/>
-    # 
-    # Pikachu's [[Gigantamax]] form is most likely a reference to its earlier, more rotund design from [[Generation I|Generations I]] and {{gen|II}}.
-
-    # ====Name origin====
-    # Pikachu is a combination of ピカピカ ''pikapika'' (onomatopoeia for sparkle) and チューチュー ''chūchū'' (the sound of a mouse squeaking).<ref>[http://edition.cnn.com/ASIANOW/time/magazine/99/1122/pokemon6.fullinterview2.html TIME - The Ultimate Game Freak]</ref>
-
-    # ==In other languages==
-    # ...
